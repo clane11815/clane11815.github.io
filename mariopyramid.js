2020dad8ca623e0b814d2df7ad82df2438cdcaaf
Caleb Lane <clane11815@lakelandcollege.edu>
2021-01-28 02:54:12 +0000

    added Mario Pyramid project

---
+++ mariopyramid.js
@@ -0,0 +1,37 @@
+
+printPyramid(5);
+// TODO #2
+// Take in user input for the height
+
+
+/*
+ * printPyramid
+ *
+ * Prints to the console a pyramid of '#' characters of the specified height
+ * For example, if height is 5, the console will look like this:
+ *          ##
+ *         ###
+ *        ####
+ *       #####
+ *      ######
+ */
+function printPyramid(height) {
+    // console.log("Uh oh... the pyramid is under construction.");
+    // console.log("Check back soon, our developers are hard at work as we speak!");
+
+    // TODO #1
+    // print that pyramid!
+  
+  var x;
+  var y;
+  let space = "           ";
+  let block = "#";
+  for (x = 1; x <= height; x++) {
+    block = block + "#";
+    space = space.substring(1);
+    console.log(space + block);
+  }
+
+    
+
+}
