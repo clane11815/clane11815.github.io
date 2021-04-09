d8a0c732af7ec8a61d7db8adae3a94104e8eecf4
Caleb Lane <>
2021-04-09 15:40:33 +0000

    allows user to enter pyramid height, which can now be any number

--- mariopyramid.js
+++ mariopyramid.js
@@ -1,38 +1,28 @@
+document.querySelector('#button').addEventListener("click", determineHeightAndThenDrawPyramid);
 
-printPyramid(10);
-// TODO #2
-// Take in user input for the height
+function determineHeightAndThenDrawPyramid() {
+  let userheight = document.querySelector('#height').value;
+  printPyramid(userheight);
+}
 
-
-/*
- * printPyramid
- *
- * Prints to the console a pyramid of '#' characters of the specified height
- * For example, if height is 5, the console will look like this:
- *          ##
- *         ###
- *        ####
- *       #####
- *      ######
- */
-function printPyramid(height) {
-    // console.log("Uh oh... the pyramid is under construction.");
-    // console.log("Check back soon, our developers are hard at work as we speak!");
-
-    // TODO #1
-    // print that pyramid!
-  
+function printPyramid(userheight) {
   var x;
   var y;
-  let space = "...........";
+  var rowStr;
+  let space = ".";
   let block = "#";
-  for (x = 1; x <= height; x++) {
-    block = block + "#";
-    space = space.substring(1);
-    var rowStr = (space + block);
-    console.log(rowStr);
+  //clears out the div
+  let element = document.getElementById("pyramid");
+  element.innerHTML = "";
+  //building
+  for (x = 1; x <= userheight; x++) {
+    rowStr = space.repeat(userheight-x) + "#" + block.repeat(x);
+    //building a row of the pyramid as a p element
+    let para = document.createElement("p");
+    let node = document.createTextNode(rowStr);
+    para.appendChild(node);
+    //adding the p element to the div
+    let element = document.getElementById("pyramid");
+    element.appendChild(para);
   }
-
-    
-
 }
