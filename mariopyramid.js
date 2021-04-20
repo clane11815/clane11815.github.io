c415d7158e1b0a14c703ce67682fac02d98a7a7f
Caleb Lane <>
2021-04-20 19:56:37 +0000

    user can select brick symbol, use slider to set pyramid height

--- mariopyramid.js
+++ mariopyramid.js
@@ -1,8 +1,12 @@
-document.querySelector('#button').addEventListener("click", determineHeightAndThenDrawPyramid);
+document.querySelector('#brick').addEventListener("onchange", determineHeightAndThenDrawPyramid);
+
+let heightvalue = document.getElementById("heightvalue");
+heightvalue.innerHTML = document.querySelector('#height').value;
 
 function determineHeightAndThenDrawPyramid() {
   let userheight = document.querySelector('#height').value;
   printPyramid(userheight);
+  heightvalue.innerHTML = userheight;
 }
 
 function printPyramid(userheight) {
@@ -10,13 +14,13 @@ function printPyramid(userheight) {
   var y;
   var rowStr;
   let space = ".";
-  let block = "#";
+  let block = document.querySelector('#brick').value;
   //clears out the div
   let element = document.getElementById("pyramid");
   element.innerHTML = "";
   //building
   for (x = 1; x <= userheight; x++) {
-    rowStr = space.repeat(userheight-x) + "#" + block.repeat(x);
+    rowStr = space.repeat(userheight-x) + document.querySelector('#brick').value + block.repeat(x);
     //building a row of the pyramid as a p element
     let para = document.createElement("p");
     let node = document.createTextNode(rowStr);
